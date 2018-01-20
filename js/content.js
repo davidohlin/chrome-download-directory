66f107483d7b3da11b20f46142403c4ed088664c
davidohlin <hello@davidohl.in>
2018-01-20 17:27:28 +0100

    wrap content.js in anonymous function

--- js/content.js
+++ js/content.js
@@ -1,24 +1,28 @@
-function createButton() {
-	const buttons = document.querySelector('.BtnGroup.float-right');
-	const isSubfolder = document.querySelector('.up-tree') !== null;
-	const buttonExists = document.querySelector('#chrome-dd-button') !== null;
+(function() {
+
+	function createButton() {
+		const buttons = document.querySelector('.BtnGroup.float-right');
+		const isSubfolder = document.querySelector('.up-tree') !== null;
+		const buttonExists = document.querySelector('#chrome-dd-button') !== null;
+		
+		if (isSubfolder && !buttonExists) {
+			const button = document.createElement('a');
 	
-	if (isSubfolder && !buttonExists) {
-		const button = document.createElement('a');
-
-		button.id = 'chrome-dd-button';
-		button.classList = 'btn btn-sm BtnGroup-item'
-		button.innerText = 'Download folder';
-		button.href = `https://download-directory.github.io?url=${window.location.href}`;
-
-		buttons.append(button);
+			button.id = 'chrome-dd-button';
+			button.classList = 'btn btn-sm BtnGroup-item'
+			button.innerText = 'Download folder';
+			button.href = `https://download-directory.github.io?url=${window.location.href}`;
+	
+			buttons.append(button);
+		}
 	}
-}
 
-createButton();
+	createButton();
 
-chrome.runtime.onMessage.addListener(data => {
-	if (data.action === 'locationChange') {
-		createButton();
-	}
-});
+	chrome.runtime.onMessage.addListener(data => {
+		if (data.action === 'locationChange') {
+			createButton();
+		}
+	});
+	
+}());
